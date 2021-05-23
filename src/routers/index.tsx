89a33ba7817de41f1bf78ea0d5f42efb6ed9e3b8
Minkyung Lee <gooogyeong@gmail.com>
2021-05-24 00:01:32 +0900

    feature: added get, post post methods

---
+++ src/routers/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import MainPage from '../pages/MainPage'
+import WritePage from '../pages/WritePage'
+
+function AppRouter (): JSX.Element {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={MainPage}/>
+        <Route path="/write" component={WritePage}/>
+      </Switch>
+    </Router>
+  )
+}
+
+export default AppRouter
---
+++ src/types/posts.ts
@@ -0,0 +1,7 @@
+export type PostPayload = {
+  title: string;
+  content: string;
+  createdAt: string;
+}
+
+export type Post = PostPayload
