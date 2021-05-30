c00547be8d5ebc6caceb9ab11fa576ada190f80e
Minkyung Lee <gooogyeong@gmail.com>
2021-05-30 22:07:56 +0900

    chore: added login page draft, refactoring: cleaned up components repo

--- src/components/write/ImageUploadModal.tsx
+++ src/components/write/ImageUploadModal.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useRef, Dispatch, SetStateAction } from 'react'
-import styled from 'styled-components'
 import Modal from '@/components/fragmented/Modal'
 
 type ImageUploadModalProps = {
--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import MainPage from '@/pages/MainPage'
+import LoginPage from '@/pages/LoginPage'
 import WritePage from '@/pages/WritePage'
 
 function AppRouter (): JSX.Element {
@@ -8,6 +9,7 @@ function AppRouter (): JSX.Element {
     <Router>
       <Switch>
         <Route exact path="/" component={MainPage}/>
+        <Route path="/login" component={LoginPage}/>
         <Route path="/write" component={WritePage}/>
       </Switch>
     </Router>
