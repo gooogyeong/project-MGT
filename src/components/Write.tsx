e249e5e46275552cde18794b9311304c4d28159f
Minkyung Lee <gooogyeong@gmail.com>
2021-05-30 21:04:25 +0900

    feature: upload and retrieve video file with firebase storage

--- src/components/Write.tsx
+++ src/components/Write.tsx
@@ -1,57 +1,10 @@
-import React, { useState } from 'react'
-import { postPost } from '@/services/posts'
+import React from 'react'
 import Editor from '@/components/Editor'
-import ImageUploadModal from '@/components/ImageUploadModal'
 
 const Write: React.FC = () => {
-
-  const [title, setTitle] = useState('')
-  const [content, setContent] = useState('')
-
-  // const submit = async () => {
-  //   const payload = {
-  //     title,
-  //     content,
-  //     createdAt: new Date().toString()
-  //   }
-  //   try {
-  //     await postPost(payload)
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
-
-  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTitle(e.target.value)
-  }
-
-  const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setContent(e.target.value)
-  }
-
   return (
     <div>
       <Editor />
-      {/*<ImageUploadModal />*/}
-      {/*<label>제목</label>*/}
-      {/*<input*/}
-      {/*  onChange={handleTitleChange}*/}
-      {/*  spellCheck={false}*/}
-      {/*/>*/}
-      {/*<div>*/}
-      {/*<textarea*/}
-      {/*  rows={15}*/}
-      {/*  onChange={handleContentChange}*/}
-      {/*  spellCheck={false}*/}
-      {/*/>*/}
-      {/*</div>*/}
-      {/*<div>*/}
-        {/*<button*/}
-        {/*  onClick={submit}*/}
-        {/*>*/}
-        {/*  submit*/}
-        {/*</button>*/}
-      {/*</div>*/}
     </div>
   )
 }
--- src/services/firebase/index.ts
+++ src/services/firebase/index.ts
@@ -1,6 +1,8 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 import config from '../../../env.json'
 
 initializeApp(config.firebaseConfig)
 export const db = getFirestore()
+export const storage = getStorage()
