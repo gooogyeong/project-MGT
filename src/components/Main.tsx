cdf70aa3e47ea9238db0585185a3ad2390f50520
Minkyung Lee <gooogyeong@gmail.com>
2021-06-12 21:24:52 +0900

    feature: search filter by author and tag, order by createdAt

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,56 +1,139 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import ReactHtmlParser from 'react-html-parser'
-import { getPosts } from '@/services/posts'
+import { getPosts, GetPostsPaylod } from '@/services/posts'
 import { Post } from '@/types/posts'
 import { signOut } from 'firebase/auth'
 import { auth } from '@/services/firebase'
 import styled from 'styled-components'
+import { storeContext } from '@/stores/context'
+import { useObserver } from 'mobx-react-lite'
+import { Admin } from '@/types/admin'
+import { Order } from '@/utils/enum'
+import Tag from '@/components/fragmented/Tag'
+import { getTags } from '@/services/tags'
+import { Tag as TagType } from '@/types/tags'
 
-const Feed: React.FC = () => {
+const Feed = (): JSX.Element => {
+
+  const store = React.useContext(storeContext)
+
+  const searchBarInput = useRef<HTMLInputElement>(null)
 
   const [posts, setPosts] = useState([] as Post[])
+  const [admins, setAdmins] = useState([] as Admin[])
+  const [getPostsPayload, setGetPostsPayload] = useState({
+    createdAt: 'desc',
+    authorUid: '',
+    tag: null
+  } as GetPostsPaylod)
 
   useEffect(() => {
-    const getPost = async () => {
+    const getPostList = async () => {
       try {
-        const posts = await getPosts()
+        const posts = await getPosts(getPostsPayload)
         setPosts(posts as Post[])
       } catch (error) {
         console.log(error)
       }
     }
-    getPost()
+    getPostList()
+  }, [JSON.stringify(getPostsPayload)])
+
+  useEffect(() => {
+    const getAdmins = async () => {
+      try {
+        const res = await store?.admin.getAdmins()
+        setAdmins(res as Admin[])
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getAdmins()
   }, [])
 
-  const handleSignOut = () => {
-    signOut(auth).then(() => {
-      console.log('signout successful')
-    }).catch((error) => {
-      console.log(error)
-    })
-  }
+  const [tags, setTags] = useState([] as TagType[])
+  const [searchTag, setSearchTag] = useState(null as (null | TagType))
 
-  return (
-    <MGTMain>
-      <button onClick={handleSignOut}>sign out</button>
-      post lists:
-      {posts.map((post, postIndex) => {
-        return (
-          <div key={postIndex}>
-            <div>author: {post.author}</div>
-            <div>createdAt: {post.createdAt.toString()}</div>
-            <div>title: {post.title}</div>
-            <div>{ReactHtmlParser(post.content).map((content => content))}</div>
-          </div>
-        )
-      })}
-    </MGTMain>
-  )
+  useEffect(() => {
+    const getTagList = async () => {
+      try {
+        const tags = await getTags()
+        console.log(tags)
+        setTags(tags as TagType[])
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getTagList()
+  }, [])
+
+  return useObserver(() => {
+    const handleSignOut = () => {
+      signOut(auth)
+        .then(() => {
+          console.log('signout successful')
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+
+    const handleCreatedAtOrderSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+      setGetPostsPayload({ ...getPostsPayload, createdAt: e.target.value as Order })
+    }
+
+    const handleAuthorSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+      setGetPostsPayload({ ...getPostsPayload, authorUid: e.target.value as Order })
+    }
+
+    const handleSearchClick = () => {
+      setGetPostsPayload({ ...getPostsPayload, tag: searchTag })
+    }
+
+    return (
+      <MGTMain>
+        <button onClick={handleSignOut}>sign out</button>
+        <div>
+          {/*TODO: searchbar 분리*/}
+          <div>태그: {tags.map((tag, tagIndex) => <Tag tag={tag} key={tagIndex} setTag={setSearchTag}/>)}</div>
+          <input
+            ref={searchBarInput}
+            spellCheck={false}
+            defaultValue={searchTag ? searchTag.name : ''}
+          />
+          <button onClick={handleSearchClick}>태그 검색</button>
+        </div>
+        <h3>post lists:</h3>
+        <select onChange={handleCreatedAtOrderSelect}>
+          {/*TODO: enum으로 만들 것*/}
+          <option value={Order.DESC}>최신순</option>
+          <option value={Order.ASC}>오래된순</option>
+        </select>
+        <select onChange={handleAuthorSelect}>
+          <option value="">전체 글쓴이</option>
+          {admins.map((admin, adminIndex) => {
+            return (<option value={admin.uid} key={adminIndex}>{admin.nickName}</option>)
+          })}
+        </select>
+        {posts.map((post, postIndex) => {
+          return (
+            <div key={postIndex}>
+              <div>author: {post.author}</div>
+              <div>createdAt: {post.createdAt.toString()}</div>
+              <div>title: {post.title}</div>
+              <div>{ReactHtmlParser(post.content).map((content => content))}</div>
+              <div>태그: {post.tags.map((tag, tagIndex) => <Tag key={tagIndex} tag={tag}/>)}</div>
+            </div>
+          )
+        })}
+      </MGTMain>
+    )
+  })
 }
 
 const MGTMain = styled.div`
   // TODO: table 스타일 최대한 inline으로
-  // TODO: table style 공통으로 관리 
+  // TODO: table style 공통으로 관리
   table {
     border-collapse: collapse;
     table-layout: fixed;
--- src/services/posts/index.ts
+++ src/services/posts/index.ts
@@ -1,15 +1,40 @@
 import { db } from '@/services/firebase'
+import { query, orderBy, where } from 'firebase/firestore'
 import { collection, addDoc, getDocs } from 'firebase/firestore'
 import { Post, PostPayload } from '@/types/posts'
+import { Order } from '@/utils/enum'
+import { Tag } from '@/types/tags'
 
-export const getPosts = async () => {
+export type GetPostsPaylod = {
+  createdAt: Order;
+  authorUid: string;
+  tag: null | Tag;
+}
+
+const postRef = collection(db, 'posts')
+
+export const getPosts = (payload: GetPostsPaylod) => {
   return new Promise((resolve, reject) => {
-    getDocs(collection(db, 'posts'))
+    const orderByCreatedAt = orderBy('createdAt', payload.createdAt)
+    let getPostsQuery
+    if (payload.authorUid || payload.tag) {
+      const whereAuthorUid = where('authorUid', '==', payload.authorUid)
+      const whereTag = where('tags', 'array-contains', payload.tag)
+      if (payload.authorUid && payload.tag) {
+        getPostsQuery = query(postRef, whereAuthorUid, whereTag, orderByCreatedAt)
+      } else {
+        getPostsQuery = query(postRef, payload.authorUid ? whereAuthorUid : whereTag, orderByCreatedAt)
+      }
+    } else {
+      getPostsQuery = query(postRef, orderByCreatedAt)
+    }
+    getDocs(getPostsQuery)
       .then((res) => {
         const posts = [] as Post[]
         res.forEach((doc) => {
           posts.push(doc.data() as Post)
         })
+        console.log(posts)
         resolve(posts)
       })
       .catch((error) => {
@@ -22,8 +47,8 @@ export const postPost = (payload: PostPayload) => {
   return new Promise((resolve, reject) => {
     addDoc(collection(db, 'posts'), payload)
       .then((res) => {
-      resolve(res)
-    }).catch((error) => {
+        resolve(res)
+      }).catch((error) => {
       reject(error)
     })
   })
