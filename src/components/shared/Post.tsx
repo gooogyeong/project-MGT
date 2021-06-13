2fb566f7b061c1f8ffdbe117646278e419bbb0e5
Minkyung Lee <gooogyeong@gmail.com>
2021-06-13 21:21:39 +0900

    feature: delete post draft

--- src/components/shared/Post.tsx
+++ src/components/shared/Post.tsx
@@ -1,23 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import ReactHtmlParser from 'react-html-parser'
 import { Post as PostType } from '@/types/posts'
 import Tag from '@/components/shared/Tag'
 import styled from 'styled-components'
+import { deletePost } from '@/services/posts'
+import { storeContext } from '@/stores/context'
+import { useObserver } from 'mobx-react-lite'
 
 type PostProps = {
   post: PostType;
 }
 
 const Post = (props: PostProps) => {
-  return (
-    <MGTPost>
-      <div>author: {props.post.author}</div>
-      {props.post.createdAt ? <div>createdAt: {props.post.createdAt.toString()}</div> : null}
-      <div>title: {props.post.title}</div>
-      <div>{ReactHtmlParser(props.post.content).map((content => content))}</div>
-      {props.post.tags.map((tag, tagIndex) => <Tag key={tagIndex} tag={tag}/>)}
-    </MGTPost>
-  )
+  const store = useContext(storeContext)
+
+  const handleDeletePostClick = async () => {
+    if (window.confirm('정말 삭제하시겠습니까?')) {
+      await deletePost(props.post.id)
+      if (store) await store.post.getPosts()
+      alert('삭제되었습니다')
+    }
+  }
+
+  return useObserver(() => {
+    return (
+      <MGTPost>
+        <div>
+          <button onClick={handleDeletePostClick}>포스트 삭제</button>
+        </div>
+        <div>author: {props.post.author}</div>
+        {props.post.createdAt ? <div>createdAt: {props.post.createdAt.toString()}</div> : null}
+        <div>title: {props.post.title}</div>
+        <div>{ReactHtmlParser(props.post.content).map((content => content))}</div>
+        {props.post.tags.map((tag, tagIndex) => <Tag key={tagIndex} tag={tag}/>)}
+      </MGTPost>
+    )
+  })
 }
 
 const MGTPost = styled.div`
--- src/utils/hooks.ts
+++
@@ -1,7 +0,0 @@
-import { useRef } from 'react'
-
-export const useComponentWillMount = (func: () => void) => {
-  const willMount = useRef(true)
-  if (willMount.current) func()
-  willMount.current = false
-}
