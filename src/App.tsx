4f8f0a68d310a373990fe6871f07785d001a557f
Minkyung Lee <gooogyeong@gmail.com>
2021-07-11 15:15:21 +0900

    chore: deleted comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,9 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import AppRouter from '@/routers'
 import { AuthProvider } from '@/components/Auth'
 import StoreProvider from '@/stores/context'
-import { postIndex } from '@/services/algolia'
-import { PostPayloadKey } from '@/types/posts'
 
 function App () {
-
-  // TODO: uncomment
-  // useEffect(() => {
-  //   const setSearchSetting = async () => {
-  //     try {
-  // //       // TODO: 얘 어디로 옮겨야 .. ??
-  //       await postIndex.setSettings({
-  //         attributesForFaceting: [
-  //           PostPayloadKey.authorUid,
-  //           PostPayloadKey.categoryId,
-  //           PostPayloadKey.title,
-  //           PostPayloadKey.content,
-  //           PostPayloadKey.tags
-  //         ],
-  //         ranking: [
-  //           `desc(${PostPayloadKey.createdAt})`
-  //         ]
-  //       })
-  //     } catch (error) {
-  //       console.log(error)
-  //     }
-  //   }
-  //   setSearchSetting()
-  // }, [])
 
   return (
     <StoreProvider>
