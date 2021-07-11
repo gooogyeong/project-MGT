3f47dbf93bf14eba3a85546ded332b698bf6eec5
Minkyung Lee <gooogyeong@gmail.com>
2021-07-11 15:01:07 +0900

    chore: commented setSettings method

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,28 @@ import { PostPayloadKey } from '@/types/posts'
 function App () {
 
   // TODO: uncomment
-  useEffect(() => {
-    const setSearchSetting = async () => {
-      try {
-  //       // TODO: 얘 어디로 옮겨야 .. ??
-        await postIndex.setSettings({
-          attributesForFaceting: [
-            PostPayloadKey.authorUid,
-            PostPayloadKey.categoryId,
-            PostPayloadKey.title,
-            PostPayloadKey.content,
-            PostPayloadKey.tags
-          ],
-          ranking: [
-            `desc(${PostPayloadKey.createdAt})`
-          ]
-        })
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    setSearchSetting()
-  }, [])
+  // useEffect(() => {
+  //   const setSearchSetting = async () => {
+  //     try {
+  // //       // TODO: 얘 어디로 옮겨야 .. ??
+  //       await postIndex.setSettings({
+  //         attributesForFaceting: [
+  //           PostPayloadKey.authorUid,
+  //           PostPayloadKey.categoryId,
+  //           PostPayloadKey.title,
+  //           PostPayloadKey.content,
+  //           PostPayloadKey.tags
+  //         ],
+  //         ranking: [
+  //           `desc(${PostPayloadKey.createdAt})`
+  //         ]
+  //       })
+  //     } catch (error) {
+  //       console.log(error)
+  //     }
+  //   }
+  //   setSearchSetting()
+  // }, [])
 
   return (
     <StoreProvider>
