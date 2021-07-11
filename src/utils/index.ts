874b79fef39b3ba319919c60e502afad9c28e552
Minkyung Lee <gooogyeong@gmail.com>
2021-07-11 13:18:04 +0900

    squash! feature: search filter by author,category,year,month and tags

---
+++ src/utils/index.ts
@@ -0,0 +1,35 @@
+import { RequestOptions } from '@algolia/transporter'
+import { SearchOptions } from '@algolia/client-search'
+import { PostPayloadKey } from '@/types/posts'
+import { SearchRange } from '@/types/posts'
+
+const getFilterStr = (searchOptionFlags: Record<string, string | SearchRange>) => {
+  let filterStr = ''
+  if (searchOptionFlags.authorUid) {
+    filterStr += `${PostPayloadKey.authorUid}:"${searchOptionFlags.authorUid}"`
+  }
+  if (searchOptionFlags.categoryId) {
+    filterStr += `${filterStr.length ? ' AND ' : ''}${PostPayloadKey.categoryId}:"${searchOptionFlags.categoryId}"`
+  }
+  if (searchOptionFlags.tagId) {
+    // TODO: 언제나 단독조건?
+    filterStr += `${filterStr.length ? ' AND ' : ''}${PostPayloadKey.tags}:"${searchOptionFlags.tagId}"`
+  }
+  if (searchOptionFlags.searchRange) {
+    filterStr += `${filterStr.length ? ' AND' : ''} createdAt:${(searchOptionFlags.searchRange as SearchRange).from} TO ${(searchOptionFlags.searchRange as SearchRange).to}`
+  }
+  return filterStr
+}
+
+// TODO: 타입 정교화
+export const getSearchOptions = (searchOptionFlags: Record<PostPayloadKey, string>, page: number, hitsPerPage: number): RequestOptions & SearchOptions => {
+  const searchOptions = {
+    filters: getFilterStr(searchOptionFlags),
+    page: page - 1,
+    hitsPerPage
+  } as Partial<RequestOptions & SearchOptions>
+  Object.keys(searchOptions).forEach((key) => {
+    if (key !== 'page' && !searchOptions[key]) delete searchOptions[key]
+  })
+  return searchOptions
+}
