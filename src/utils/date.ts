68dd7975563934d7eaea785359c5b6d024c08fae
Minkyung Lee <gooogyeong@gmail.com>
2021-07-11 13:19:15 +0900

    chore: added date util

---
+++ src/utils/date.ts
@@ -0,0 +1,16 @@
+import { getYear } from 'date-fns'
+
+export const months = new Array(12).fill(true).map((month, monthIdx) => {
+  return monthIdx + 1
+})
+
+const thisYear = getYear(new Date())
+
+export const years = new Array(thisYear - 2021 + 1).fill(true).map((year, yearIdx) => {
+  return 2021 + yearIdx
+})
+
+export const yyMMddDash = 'yy-MM-dd'
+export const yyMMddDot = 'yy.MM.dd'
+
+export const HHmm = 'HH:mm'
