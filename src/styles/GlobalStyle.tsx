ac13afa0a611e6a6857b1fd94df713e0d7149249
Minkyung Lee <gooogyeong@gmail.com>
2021-07-13 01:03:54 +0900

    style: applied global style

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+html {
+font-size: 10px;
+font-family: 'Noto Serif KR', sans-serif;
+}
+`
+
+export default GlobalStyle
