77d1d880ccdea00332721c6b329634ce335c30bd
Minkyung Lee <gooogyeong@gmail.com>
2021-07-16 01:24:33 +0900

    chore: added footer

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,24 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
 import Header from '@/components/Header'
+import NavBar from '@/components/NavBar'
+import Footer from '@/components/Footer'
 
 type MainLayoutProps = {
   children: JSX.Element;
 }
 
-const MGTMainLayout = styled.div`
-padding: 6.8rem 7.6rem;
-`
-
 const MainLayout = (props: MainLayoutProps) => {
   return (
     <MGTMainLayout>
       <Header/>
+      <NavBar />
       <div>
         {props.children}
       </div>
+      <Footer />
     </MGTMainLayout>
   )
 }
+
+const MGTMainLayout = styled.div`
+margin: 6.8rem 7.6rem;
+border: 1px dotted red;
+`
 
 export default MainLayout
