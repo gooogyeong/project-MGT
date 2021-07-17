10d37a8089de88ba9bcc204499ce2fd01b7df552
Minkyung Lee <gooogyeong@gmail.com>
2021-07-17 23:42:26 +0900

    feature: styled navbar, refactoring: brought style code to below

---
+++ src/components/shared/SearchBar.tsx
@@ -0,0 +1,55 @@
+import styled from 'styled-components'
+import React, { useRef } from 'react'
+import { storeContext } from '@/stores/context'
+
+type SearchBarProps = {
+  handleSearchButtonClick: () => Promise<void>;
+}
+
+const SearchBar = (props: SearchBarProps) => {
+
+  const searchBarInput = useRef<HTMLInputElement>(null)
+
+  const store = React.useContext(storeContext)
+
+  const handleSearchButtonClick = () => {
+    if (searchBarInput.current?.value || searchBarInput.current?.value === '') {
+      store?.post.setSearchKeyword(searchBarInput.current.value)
+    }
+    props.handleSearchButtonClick()
+  }
+
+  const handleInputKeyup = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') handleSearchButtonClick()
+  }
+
+  return (
+    <MGTSearchBar className="searchbar">
+      <input
+        ref={searchBarInput}
+        spellCheck={false}
+        onKeyUp={handleInputKeyup}
+      />
+      <button onClick={handleSearchButtonClick}>검색</button>
+    </MGTSearchBar>
+  )
+}
+
+const MGTSearchBar = styled.div`
+display: flex;
+font-size: 1.8rem;
+input {
+border: 1px dotted red;
+padding: 0 0 0 1.6rem;
+font-size: 1.8rem;
+font-family: 'Noto Serif KR', sans-serif;
+}
+button {
+background-color: red;
+color: white;
+font-size: 1.8rem;
+padding: 0 2rem;
+}
+`
+
+export default SearchBar
