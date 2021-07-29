629c3fffc657823224b210de7307c9672fa0c18e
Minkyung Lee <gooogyeong@gmail.com>
2021-07-29 23:46:59 +0900

    feature: 소개 메뉴 추가

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import MainLayout from '@/layouts/MainLayout'
 import MainView from '@/views/MainView'
+import IntroView from '@/views/IntroView'
 import PostListView from '@/views/post/PostListView'
 import PostDetailView from '@/views/post/PostDetailView'
 import LoginView from '@/views/LoginView'
 import WriteView from '@/views/WriteView'
-import EditView from '@/views/EditView'
+// import EditView from '@/views/EditView'
 import PrivateRoute from '@/routers/privateRoute'
 
 function AppRouter (): JSX.Element {
@@ -15,11 +16,13 @@ function AppRouter (): JSX.Element {
       <MainLayout>
         <Switch>
           <Route exact path="/" component={MainView}/>
+          <Route exact path="/intro" component={IntroView}/>
           <Route exact path="/post/list" component={PostListView}/>
           <Route exact path="/post/:id" component={PostDetailView}/>
           <Route path="/login" component={LoginView}/>
           <PrivateRoute exact path="/write" component={WriteView}/>
-          <PrivateRoute exact path="/write/:postId" component={EditView}/>
+          <PrivateRoute exact path="/write/:postId" component={WriteView}/>
+          {/*<PrivateRoute exact path="/write/:postId" component={EditView}/>*/}
         </Switch>
       </MainLayout>
     </Router>
