f7717bedee92f92bc38c67c812703a5ecf2b276b
Minkyung Lee <gooogyeong@gmail.com>
2021-07-31 13:10:13 +0900

    style: improved main feed style

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { signOut } from 'firebase/auth'
 import { auth } from '@/services/firebase'
@@ -13,12 +13,16 @@ import ReactHtmlParser from 'react-html-parser'
 import Button from '@/components/shared/Button'
 import Tag from '@/components/shared/Tag'
 import { format } from 'date-fns'
+import { Options } from 'react-html-parser'
+import { Node } from '@/types'
 
 const Feed = (): JSX.Element => {
 
   const store = React.useContext(storeContext)
 
   const history = useHistory()
+
+  const [thumbnailArr, setThumbnailArr] = useState([undefined, undefined] as (Node | undefined)[])
 
   useEffect(() => {
     const getMainPost = async () => {
@@ -55,6 +59,32 @@ const Feed = (): JSX.Element => {
     history.push(`/post/${post.objectID}`)
   }
 
+  useEffect(() => {
+    store?.post.authorLatestPosts.forEach((post, postIdx) => {
+      processContent(postIdx)
+    })
+  }, [])
+
+  const processContent = (postIdx: number) => {
+    const content = store?.post.authorLatestPosts[postIdx].content as string
+    const transform = (node: Node) => {
+      if (node.type === 'tag' && node.name === 'img') {
+        if (thumbnailArr[postIdx] === undefined) {
+          const newThumbnailArr = [...thumbnailArr]
+          newThumbnailArr[postIdx] = node
+          setThumbnailArr(newThumbnailArr)
+        }
+        return null
+      }
+
+      if (node.type === 'tag' && node.name === 'video') {
+        return null
+      }
+    }
+    const options = { transform } as Options
+    return ReactHtmlParser(content, options)
+  }
+
   return useObserver(() => {
     const handleSignOut = () => {
       signOut(auth)
@@ -70,50 +100,51 @@ const Feed = (): JSX.Element => {
       <MGTMain>
         <ContentHeader text={`"${thisYear}년 그로테스크의 해"`}/>
         <div className="main__body">
-          <div className="content__wrapper">
-            <div className="label">{store?.post.authorLatestPosts[0]?.author}</div>
-            <div className="post">
-              <div className="post__title">{store?.post.authorLatestPosts[0]?.title}</div>
-              <div className="post__content">
-                <div className="post__content__header">
-                <span>
-                {store?.post.authorLatestPosts[1]?.createdAt ? format(new Date(store?.post.authorLatestPosts[1]?.createdAt), yyyyMMddDot) : null}
-                </span>
-                  <span>
-                {store?.post.authorLatestPosts[1]?.tags.map((tag, tagIdx) => {
-                  return <Tag key={tagIdx} tag={tag}/>
-                })}
-                </span>
-                </div>
-                <div>{ReactHtmlParser(store?.post.authorLatestPosts[0]?.content as string)}</div>
-                <div className="button__container">
-                  <Button buttonText="더 봐" onClick={() => goToPostDetail(0)}/>
+          {store?.post.authorLatestPosts.map((post, postIdx) => {
+            return (
+              <div key={postIdx} className="content__wrapper">
+                <div className="label">{post.author}</div>
+                <div className="post">
+                  <div className="post__title">
+                    {post.title}
+                  </div>
+                  <div className="post__content">
+                    <div className="post__content__header">
+                      <span>
+                      {post.createdAt ? format(new Date(post.createdAt), yyyyMMddDot) : null}
+                      </span>
+                      <span>
+                      {post?.tags.map((tag, tagIdx) => {
+                        return <Tag key={tagIdx} tag={tag}/>
+                      })}
+                      </span>
+                    </div>
+                    <div className={`post__content__body ${!postIdx ? 'left' : 'right'}`}>
+                      <div className="main-text">
+                        <div className="text">
+                          <div>{processContent(postIdx)}</div>
+                        </div>
+                      </div>
+                      {thumbnailArr[postIdx] ? (
+                        <div className="thumbnail__container">
+                          <img
+                            src={thumbnailArr[postIdx]?.attribs?.src}
+                            alt={thumbnailArr[postIdx]?.attribs?.alt}
+                          />
+                        </div>
+                      ) : null}
+                      <div className="button__container">
+                        <Button buttonText="더 봐" onClick={() => goToPostDetail(postIdx)}/>
+                      </div>
+                    </div>
+                    {/*<div className="button__container">*/}
+                    {/*  <Button buttonText="더 봐" onClick={() => goToPostDetail(postIdx)}/>*/}
+                    {/*</div>*/}
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="content__wrapper">
-            <div className="label">{store?.post.authorLatestPosts[1]?.author}</div>
-            <div className="post">
-              <div className="post__title">{store?.post.authorLatestPosts[1]?.title}</div>
-              <div className="post__content">
-                <div className="post__content__header">
-                <span>
-                {store?.post.authorLatestPosts[1]?.createdAt ? format(new Date(store?.post.authorLatestPosts[1]?.createdAt), yyyyMMddDot) : null}
-                </span>
-                  <span>
-                {store?.post.authorLatestPosts[1]?.tags.map((tag, tagIdx) => {
-                  return <Tag key={tagIdx} tag={tag}/>
-                })}
-                </span>
-                </div>
-                <div>{ReactHtmlParser(store?.post.authorLatestPosts[1]?.content as string)}</div>
-                <div className="button__container">
-                  <Button buttonText="더 봐" onClick={() => goToPostDetail(1)}/>
-                </div>
-              </div>
-            </div>
-          </div>
+            )
+          })}
         </div>
         {/*TODO: 복구*/}
         {/*<button onClick={handleSignOut}>sign out</button>*/}
@@ -131,6 +162,7 @@ display: flex;
 & > div {
 &.content__wrapper {
 flex-basis: 50%;
+max-width: 50%;
 &:not(:last-child) {
 border-right: 1px dotted red;
 }
@@ -143,12 +175,21 @@ font-size: 2rem;
 &__title {
 margin-top: 0.3rem;
 background-color: blue;
-padding: 0.5rem 0;
+padding: 0.5rem 0.7rem;
 color: white;
 text-align: center;
+max-height: 2.8rem;
+white-space: nowrap;
+overflow: hidden;
+text-overflow: ellipsis;
 }
 &__content {
 padding: 0.7rem 0.8rem;
+max-height: 111.9rem;
+position: relative;
+overflow: hidden;
+display: flex;
+flex-direction: column;
 &__header {
 display: flex;
 justify-content: space-between;
@@ -162,9 +203,60 @@ margin-right: 0.4rem;
 }
 }
 }
+&__body {
+display: flex;
+flex-direction: column;
+position: relative;
+&.left {
+.thumbnail__container {
+order: 1;
+justify-content: flex-start;
+}
+.main-text { 
+order: 2;
+}
+.button__container {
+order: 3;
+}
+}
+&.right {
+.thumbnail__container {
+order: 3;
+justify-content: flex-end;
+}
+.main-text { 
+order: 1;
+}
+.button__container {
+order: 2;
+}
+}
+.thumbnail__container {
+max-height: 42.8rem;
+display: flex;
+margin: 0 -0.7rem;
+img {
+// TODO: 왜 fill이 안되는지. 그리고 이미지 사이즈에 따라 fill / cover 분기처리
+object-fit: cover;
+}
+}
+.main-text {
+.text {
+max-height: 46.9rem;
+overflow: hidden;
+text-overflow: ellipsis;
+box-shadow: white 0px -60px 40px 0 inset; 
+& > div {
+position: relative;
+z-index: -1;
+} 
+}
+}
+}
 .button__container {
 display: flex;
 justify-content: flex-end;
+margin-bottom: 2.1rem;
 }
 }
 }
