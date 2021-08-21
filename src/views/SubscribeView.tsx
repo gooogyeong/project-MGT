f8e76efde47e3adf08605f434123d0b2e5146a00
Minkyung Lee <gooogyeong@gmail.com>
2021-08-22 01:47:36 +0900

    feature: added newsletter subscription menu, chore: changed board menu title

---
+++ src/views/SubscribeView.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Subscribe from '@/components/Subscribe'
+
+const SubscribeView: React.FC = () => {
+
+  return (
+    <div>
+      <Subscribe />
+    </div>
+  )
+}
+
+export default SubscribeView
