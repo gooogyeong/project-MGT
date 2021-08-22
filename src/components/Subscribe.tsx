b3a9a32bb0d7fa5c905bf3dd81c798d118bcf52c
Minkyung Lee <gooogyeong@gmail.com>
2021-08-22 11:09:40 +0900

    fix: changed script load timing, adjusted mobile UI

--- src/components/Subscribe.tsx
+++ src/components/Subscribe.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import createDOMPurify from 'dompurify'
 import styled from 'styled-components'
 
 const DOMPurify = createDOMPurify(window)
 
 const rawHTML = `
-<link rel="stylesheet"
-      href="https://s3.ap-northeast-2.amazonaws.com/resource.stibee.com/subscribe/stb_subscribe_form_style.css">
 <div id="stb_subscribe">
     <form action="https://stibee.com/api/v1.0/lists/zTTcV1LGklcfL-mClT5VEAnWvUWBaQ==/public/subscribers"
           method="POST"
@@ -43,8 +41,9 @@ const rawHTML = `
             <div class="stb_form_modal stb_form_policy_text blind" id="stb_form_policy_modal">
                 <div class="stb_form_modal_body">
                     <h1 class="stb_form_modal_title">개인정보 수집 및 이용</h1>
-                    <p class="stb_form_modal_text">뉴스레터 발송을 위한 최소한의 개인정보를 수집하고 이용합니다.
-                        수집된 정보는 발송 외 다른 목적으로 이용되지 않으며, 서비스가 종료되거나 구독을 해지할 경우 즉시 파기됩니다.</p>
+                    <p class="stb_form_modal_text desktop">뉴스레터 발송을 위한 최소한의 개인정보를 수집하고 이용합니다.<br>수집된 정보는 발송 외 다른 목적으로 이용되지 않으며,<br>서비스가 종료되거나 구독을 해지할 경우 즉시 파기됩니다.
+                    <p class="stb_form_modal_text mobile">뉴스레터 발송을 위한 최소한의 개인정보를 수집하고 이용합니다. 수집된 정보는 발송 외 다른 목적으로 이용되지 않으며, 서비스가 종료되거나 구독을 해지할 경우 즉시 파기됩니다.
+                    </p>
                     <div class="stb_form_modal_btn">
                         <button id="stb_form_modal_close" class="stb_form_modal_close_btn"
                                 data-modal="stb_form_policy_modal"
@@ -65,18 +64,30 @@ const rawHTML = `
         </fieldset>
     </form>
 </div>
-<script src="https://www.google.com/recaptcha/api.js" async="" defer=""></script>
-<script type="text/javascript"
-        src="https://s3.ap-northeast-2.amazonaws.com/resource.stibee.com/subscribe/stb_subscribe_form.js"></script>
 `
 
-const Subscribe = () => (
-  <MGTSubscribe>
-    {<div className="stibee-form-wrapper" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(rawHTML) }}/>}
-  </MGTSubscribe>
-)
+const Subscribe = () => {
 
-export default Subscribe
+  useEffect(() => {
+    const stibeeScript = document.createElement('script')
+
+    stibeeScript.src = 'https://s3.ap-northeast-2.amazonaws.com/resource.stibee.com/subscribe/stb_subscribe_form.js'
+    stibeeScript.async = true
+
+    const recaptchaScript = document.createElement('script')
+    recaptchaScript.src = 'https://www.google.com/recaptcha/api.js'
+    recaptchaScript.async = true
+
+    document.body.appendChild(stibeeScript)
+    document.body.appendChild(recaptchaScript)
+  }, [])
+
+  return (
+    <MGTSubscribe>
+      {<div className="stibee-form-wrapper" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(rawHTML) }}/>}
+    </MGTSubscribe>
+  )
+}
 
 const MGTSubscribe = styled.div`
 display: flex;
@@ -86,13 +97,11 @@ padding: 4.7rem 0;
 
 .stibee-form-wrapper {
 flex-basis: 60%;
-}
 
 #stb_subscribe {
 padding: 4.7rem 3rem;
-}
 
-#stb_subscribe .stb_form_policy  {
+.stb_form_policy  {
     label {
     display: flex;
     align-items: center;
@@ -102,8 +111,44 @@ padding: 4.7rem 3rem;
     }
 }
 
-#stb_subscribe .stb_form_set_submit {
+#stb_form_policy_modal {
+p.stb_form_modal_text {
+white-space: pre-line !important;
+&.mobile {
+display: none;
+}
+}
+
+.stb_form_modal_btn {
 display: flex;
 justify-content: flex-end;
 }
+}
+
+.stb_form_set_submit {
+display: flex;
+justify-content: flex-end;
+}
+}
+}
+
+@media screen and (max-width: ${props => props.theme.widthMobileScreen}) {
+padding: 3.1rem 1.3rem;
+.stibee-form-wrapper {
+flex-basis: 100%;
+
+#stb_subscribe {
+p.stb_form_modal_text {
+&.mobile {
+display: block !important;
+}
+&.desktop {
+display: none;
+}
+}
+}
+}
+}
 `
+
+export default React.memo(Subscribe)
