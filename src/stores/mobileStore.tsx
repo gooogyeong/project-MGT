1fded9c21969e357f94a2737456ea060a276bdaf
Minkyung Lee <gooogyeong@gmail.com>
2021-08-31 01:31:28 +0900

    fix: tablet ui 1st draft

--- src/stores/mobileStore.tsx
+++ src/stores/mobileStore.tsx
@@ -1,14 +1,21 @@
 export type MobileStore = {
   isMobile: boolean;
+  isTablet: boolean;
   setIsMobile: (payload: boolean) => void;
+  setIsTablet: (payload: boolean) => void;
 }
 
 export const mobileStore = (): MobileStore => {
   const store: MobileStore = {
     isMobile: false,
+    isTablet: false,
 
     setIsMobile (isMobile) {
       this.isMobile = isMobile
+    },
+
+    setIsTablet (isTablet) {
+      this.isTablet = isTablet
     }
   }
   return store
