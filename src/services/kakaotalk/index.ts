d0cf1472eccd081350afbfc4a11328a569eb1395
Minkyung Lee <gooogyeong@gmail.com>
2021-09-04 20:34:18 +0900

    fix: changed meta img size

--- src/services/kakaotalk/index.ts
+++ src/services/kakaotalk/index.ts
@@ -6,7 +6,7 @@ export const share = ({ title, objectID }: { title: string; objectID: string }) 
     content: {
       title: title,
       description: '',
-      imageUrl: `${config.baseURL}/modern-grotesque-times.png`,
+      imageUrl: `${config.baseURL}/modern-grotesque-times-share.png`,
       link: {
         mobileWebUrl: `${config.baseURL}/post/${objectID}`,
         androidExecParams: 'test'
